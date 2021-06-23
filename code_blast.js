74614597f9ffe71d8956af3f319494625a5ec8a9
Auxilia-vinnarasi <83941036+Auxilia-vinnarasi@users.noreply.github.com>
2021-06-23 18:51:04 +0530

    Add files via upload

---
+++ code_blast.js
@@ -0,0 +1,11 @@
+var a = "2"<"12";
+/*when we compare two strings,the comparison happens on chacracter basis. 
+so '2' > '12' is true because the comparison will happen as '2' > '1' in alphabetical way. 
+so '2' is always greater than '1'. 
+
+Don't touch below this*/
+if (a) {
+    console.log("Code is Blasted")
+} else {
+    console.log("Diffused")
+}
