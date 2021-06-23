31a95cf6037cc3d0f1ce9639c60f0d60a7b328d1
Auxilia-vinnarasi <83941036+Auxilia-vinnarasi@users.noreply.github.com>
2021-06-23 19:13:10 +0530

    Add files via upload

---
+++ success_console.js
@@ -0,0 +1,12 @@
+let a = prompt("Enter a number?");
+//Don't modify any code below this
+if (a) {
+ console.log( 'OMG it works for any number inc 0' );
+}
+else
+{
+ console.log( "Success" );
+}
+
+/*if we put anything in promt box and click ok, it will show 'OMG it works for any number inc 0' 
+if we dont give anything in prompt box and click ok, it will give result as "Success".*/
