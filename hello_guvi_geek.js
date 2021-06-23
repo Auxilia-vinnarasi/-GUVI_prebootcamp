613667f9696eb7a514bf96151d51468f3d3cac88
Auxilia-vinnarasi <83941036+Auxilia-vinnarasi@users.noreply.github.com>
2021-06-23 17:25:39 +0530

    Add files via upload

---
+++ hello_guvi_geek.js
@@ -0,0 +1,5 @@
+let fname = 10.5;
+fname = "Guvi";
+let lname = "geek" //lname variable is not declared.
+var fullname = fname + " " + lname; //name is reserved word in js.so we cant use as a variable name here.
+alert(`hello ${fullname}`);
