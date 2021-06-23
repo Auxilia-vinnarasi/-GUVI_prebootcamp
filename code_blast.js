9aeb1a994aea525546f2b52eff69261cccf97a0f
Auxilia-vinnarasi <83941036+Auxilia-vinnarasi@users.noreply.github.com>
2021-06-23 18:57:06 +0530

    Update code_blast.js

--- code_blast.js
+++ code_blast.js
@@ -1,11 +1,21 @@
-var a = "2"<"12";
-/*when we compare two strings,the comparison happens on chacracter basis. 
-so '2' > '12' is true because the comparison will happen as '2' > '1' in alphabetical way. 
-so '2' is always greater than '1'. 
-
-Don't touch below this*/
+var a = "2">"12";
+ //Don't touch below this
 if (a) {
     console.log("Code is Blasted")
 } else {
     console.log("Diffused")
 }
+
+/*when we compare two strings,the comparison happens on chacracter basis. 
+so '2' > '12' is true because the comparison will happen as '2' > '1' in alphabetical way. 
+so the condition true and i get the result"code is blasted".*/
+**************************
+var a = "2"<"12";
+ //Don't touch below this
+if (a) {
+    console.log("Code is Blasted")
+} else {
+    console.log("Diffused")
+}
+
+/*To get result as diffused the variable should be "2"<"12;
