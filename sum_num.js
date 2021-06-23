9e32c828264acd901433567231af603561a553fa
Auxilia-vinnarasi <83941036+Auxilia-vinnarasi@users.noreply.github.com>
2021-06-23 17:40:38 +0530

    Add files via upload

---
+++ sum_num.js
@@ -0,0 +1,3 @@
+let a = prompt("First number?");
+let b = prompt("Second number?");
+alert(+ a+ +b);// (a+b)the numbers here kept as strings, after typecasting to number the result came as expected.
