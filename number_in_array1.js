58333d0cba2818ff23bac241d1b99e3fdd260734
Auxilia-vinnarasi <83941036+Auxilia-vinnarasi@users.noreply.github.com>
2021-06-24 07:24:08 +0530

    Add files via upload

---
+++ number_in_array1.js
@@ -0,0 +1,16 @@
+var numsArr = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+var new_string = "";
+ 
+for (var i = 0; i < 11; i++) 
+{
+    if(i==numsArr.length-1)
+    {
+     new_string += numsArr[i]; 
+    }
+    else
+    {
+     new_string += numsArr[i]+","
+    }
+  
+}
+console.log(new_string);
